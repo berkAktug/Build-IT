a5ec6f24e7ba6078abe262f0e178bae05e378079
BerkAktug <berkaktug07@gmail.com>
2019-11-11 15:10:35 +0300

    Add project files.

---
+++ Build-IT Character Builder/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Build_IT_Character_Builder.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
