26447b24eec340f5843791b70779f6afeb47a098
BerkAktug <berkaktug07@gmail.com>
2019-11-11 18:42:27 +0300

    added character builder page. TODO: add functionality.

---
+++ Build-IT Character Builder/Controllers/CharacterController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Build_IT_Character_Builder.Controllers
+{
+    public class CharacterController : Controller
+    {
+        public IActionResult CharacterBuilder()
+        {
+            return View();
+        }
+    }
+}
