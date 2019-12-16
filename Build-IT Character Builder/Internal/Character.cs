97b9524ca93c4be40952dd0da3151926be229cfa
dogustarman <dogustarman@gmail.com>
2019-12-16 22:23:21 +0300

    saveToExcel

--- Build-IT Character Builder/Internal/Character.cs
+++ Build-IT Character Builder/Internal/Character.cs
@@ -1,6 +1,9 @@
 ﻿using Character_Builder.Models;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 
@@ -107,6 +110,97 @@ namespace Character_Builder.Internal
 
             // Yazdir
             var tmp_character_level = newCharacter.CharacterLevel;
+        }
+
+        public void saveToExcel(NewCharacterModel character)
+        {
+            var newFile = @"DD_CharacterSheet.xlsx";
+
+            using (var fs = new FileStream(newFile, FileMode.Create, FileAccess.Write))
+            {
+
+                IWorkbook workbook = new XSSFWorkbook();
+
+                ISheet sheet1 = workbook.CreateSheet("CharacterSheet");
+
+                //D&D Banner
+                IRow row = sheet1.CreateRow(0);
+                row.Height = 30 * 80;
+                row.CreateCell(0).SetCellValue("DUNGEONS & DRAGONS");
+                sheet1.AutoSizeColumn(0);
+
+                //Character name
+                row = sheet1.CreateRow(1);
+                row.Height = 10 * 80;
+                var charName = character.CharacterName;
+                row.CreateCell(0).SetCellValue("Character Name: " + charName);
+
+                //Class & Level
+                row = sheet1.GetRow(1);
+                row.Height = 10 * 160;
+                var className = character.CharacterClass;
+                var level = character.CharacterLevel;
+                row.CreateCell(1).SetCellValue("Class & Level: " + className + "\t" + level);
+
+                //Race & Level
+                row = sheet1.CreateRow(2);
+                row.Height = 10 * 160;
+                var race = character.CharacterRace;
+                row.CreateCell(1).SetCellValue("Race: " + race);
+
+                //Attributes
+                row = sheet1.CreateRow(3);
+                row.Height = 10 * 20;
+                var Str = character.CharacterAttributes.Strength;
+                row.CreateCell(0).SetCellValue("Strength: " + Str);
+
+                row = sheet1.CreateRow(4);
+                row.Height = 10 * 20;
+                var Dex = character.CharacterAttributes.Dexterity;
+                row.CreateCell(0).SetCellValue("Dexterity: " + Dex);
+
+                row = sheet1.CreateRow(5);
+                row.Height = 10 * 20;
+                var Con = character.CharacterAttributes.Constitution;
+                row.CreateCell(0).SetCellValue("Constitution: " + Con);
+
+                row = sheet1.CreateRow(6);
+                row.Height = 10 * 20;
+                var Int = character.CharacterAttributes.Intelligence;
+                row.CreateCell(0).SetCellValue("Intelligence: " + Int);
+
+                row = sheet1.CreateRow(7);
+                row.Height = 10 * 20;
+                var Wis = character.CharacterAttributes.Wisdom;
+                row.CreateCell(0).SetCellValue("Wisdom: " + Wis);
+
+                row = sheet1.CreateRow(8);
+                row.Height = 10 * 20;
+                var Cha = character.CharacterAttributes.Charisma;
+                row.CreateCell(0).SetCellValue("Charisma: " + Cha);
+
+                //background
+                row = sheet1.GetRow(3);
+                row.Height = 10 * 20;
+                var background = character.CharacterBackground;
+                row.CreateCell(1).SetCellValue("Background: " + background);
+
+                //background
+                row = sheet1.GetRow(4);
+                row.Height = 10 * 20;
+                var proficiencies = character.CharacterProficiencies;
+                row.CreateCell(1).SetCellValue("Background: " + background);
+
+                //var style1 = workbook.CreateCellStyle();
+                //style1.FillForegroundColor = HSSFColor.Blue.Index2;
+                //style1.FillPattern = FillPattern.SolidForeground;
+
+                //var style2 = workbook.CreateCellStyle();
+                //style2.FillForegroundColor = HSSFColor.Yellow.Index2;
+                //style2.FillPattern = FillPattern.SolidForeground;
+
+                workbook.Write(fs);
+            }
         }
 
         private CharacterClassFactory ClassAssigner(CharacterClassEnumModel characterClass)
